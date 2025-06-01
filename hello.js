180fa1377b465ba764122df00d050ce21ba3d64f
Hamid Ali <164846674+hamidali3936@users.noreply.github.com>
2025-06-01 02:53:28 -0700

    Add files via upload
    My Portfolio is look on the github .

---
+++ hello.js
@@ -0,0 +1,20 @@
+function Home(){
+   alert("Please enter the data1!");
+
+}
+function About(){
+    alert("Please enter the data1!");
+
+}
+function Services(){
+    alert("Please enter the data1!");
+
+}
+function Contect_me(){
+    alert("Please enter the data1!");
+
+}
+function Live(){
+    alert("Please enter the data1!");
+
+}
